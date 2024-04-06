3e96f2e6d7596cf43ae7603a2f3f7840baddc286
alsrudursla <nuy0307@naver.com>
2024-04-07 03:01:28 +0900

    feat: add user entity

---
+++ src/main/java/BabAl/BabalServer/domain/User.java
@@ -0,0 +1,45 @@
+package BabAl.BabalServer.domain;
+
+import BabAl.BabalServer.domain.enums.UserGender;
+import BabAl.BabalServer.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 사용자 이름
+    @Column(length = 20)
+    private String name;
+
+    // 이메일
+    @Column(length = 50)
+    private String email;
+
+    // 비밀번호
+    private String password;
+
+    // 나이
+    private int age;
+
+    // 성별
+    @Enumerated(EnumType.STRING)
+    private UserGender gender;
+
+    // 키
+    private int height;
+
+    // 몸무게
+    private int weight;
+
+    // 기초대사량
+    private int bmr;
+
+}
