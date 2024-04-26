72bdc09720f3861f1523f4d8a1169a20029c3fd1
alsrudursla <nuy0307@naver.com>
2024-04-26 18:00:30 +0900

    add: health check 추가

--- src/main/java/BabAl/BabalServer/config/SecurityConfig.java
+++ src/main/java/BabAl/BabalServer/config/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/error", "/user/login", "/user/signup").permitAll()
+                        .requestMatchers("/", "/error", "/health").permitAll()
+                        .requestMatchers("/user/login", "/user/signup").permitAll()
                         .anyRequest().authenticated());
         http
                 .sessionManagement((auth) -> auth
