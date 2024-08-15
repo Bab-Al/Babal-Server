6500ed6300e95a013baf6ad05a1706f6ed509346
alsrudursla <nuy0307@naver.com>
2024-08-15 15:27:32 +0900

    build: create-drop

--- src/main/java/BabAl/BabalServer/domain/enums/FoodCategoryName.java
+++ src/main/java/BabAl/BabalServer/domain/enums/FoodCategoryName.java
@@ -1,8 +1,53 @@
 package BabAl.BabalServer.domain.enums;
 
 public enum FoodCategoryName {
-    FRUIT, SOUP, NOODLE, RICE, BREAD, RAW_VEGES, TEA, STIR_FRY, DAIRY_PRODS,
-    SEASONED_VEGETABLES, KIMCHI, STEAMED, BOILED, FRIED, PORRIDGE, PANCAKE,
-    SEASONING, FISH, NUT, PICKLED, JEOTGAL, HOTPOT, GRILLED
 
+    // 과일류
+    FRUITS,
+    // 국 및 탕류
+    SOUP,
+    // 면 및 만두류
+    NOODLE,
+    // 밥류
+    RICE,
+    // 빵 및 과자류
+    BREAD,
+    // 생채 무침류
+    RAW_VEGES,
+    // 음료 및 차류
+    BEVERAGE_TEA,
+    // 볶음류
+    STIR_FRIED,
+    // 유제품 및 빙과류
+    DAIRY_PRODS,
+    // 장아찌 절임류
+    PICKLED,
+    // 젓갈류
+    JEOTGAL,
+    // 찌개 및 전골류
+    HOTPOT,
+    // 두류, 견과 및 종실류
+    NUTS,
+    // 수,조,어,육류
+    FISH,
+    // 곡류, 서류 제품
+    GRAINS,
+    // 구이류
+    ROASTED,
+    // 장류, 양념류
+    SEASONING,
+    // 전, 적 및 부침류
+    PANCAKE,
+    // 죽 및 스프류
+    PORRIDGE,
+    // 튀김류
+    FRIED,
+    // 찜류
+    BOILED,
+    // 조림류
+    STEAMED,
+    // 김치류
+    KIMCHI,
+    // 나물, 숙채류
+    SEASONED_VEGES
 }
